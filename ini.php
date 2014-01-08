f37f44943b6155fd1490c6e711d7b891cecbab4d
hoangcongvuong <hoangcongvuong@gmail.com>
2014-01-08 21:16:06 +0700

    haha

--- ini.php
+++ ini.php
@@ -62,4 +62,6 @@ define('DB_CACHE_DIR', VNP_ROOT . DATA_DIR . '/cache/db/' );
 define('HTML_CACHE_DIR', VNP_ROOT . DATA_DIR . '/cache/template/' );
 define('XML_CONFIG_FILE', VNP_ROOT . DATA_DIR . '/site_config.xml' );
 
+//alalalalalal
+
 ?>
